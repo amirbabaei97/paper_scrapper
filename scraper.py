76e8533b36191e2bc11a70a0822531e9a03a52fa
AmirBabaei <pr.babayee@icloud.com>
2023-12-23 13:59:12 +0100

    #TODO: Error handling for the inner for loop and the while loop

--- scraper.py
+++ scraper.py
@@ -3,10 +3,9 @@ This module contains a function to search Google Scholar using Selenium and Beau
 It uses mobile emulation and can optionally use a proxy.
 """
 
-#TODO: Error handeling for the inner for loop and the while loop
-#TODO: Error handeling for the proxy list for a quick fix
-#TODO: Error handeling for the search key
-#TODO: Error handeling for the webdriver
+#TODO: Error handling for the proxy list for a quick fix
+#TODO: Error handling for the search key
+#TODO: Error handling for the webdriver
 #TODO: Use a better method for the proxy list
 
 import json
@@ -41,7 +40,7 @@ def search_scholar(search_key, proxy=None):
     driver = webdriver.Chrome(options=options, service=webdriver_service)
     if proxy:
         options.add_argument(f'--proxy-server={proxy}')
-    # open first result in priview mode
+    # open first result in preview mode
     path = 'https://scholar.google.com/scholar?hl=en&as_sdt=0%2C5&q={}&btnG='
     path = path.format(urllib.parse.quote_plus(search_key))
     driver.get(path)
@@ -52,54 +51,57 @@ def search_scholar(search_key, proxy=None):
     click_for_preview_elements[0].click()  # Click on the first preview element
 
     while cnt < len(click_for_preview_elements):
-        print(cnt)
-        # wait until the preview is loaded
-        wait = WebDriverWait(driver, 10)
-        wait.until(EC.presence_of_element_located((By.XPATH, '//div[@class="gs_qabs_panel"]')))
-        pub_infos = driver.find_elements(By.XPATH, '//div[@class="gs_qabs_panel"]')
+        try:
+            # wait until the preview is loaded
+            wait = WebDriverWait(driver, 10)
+            wait.until(EC.presence_of_element_located((By.XPATH, '//div[@class="gs_qabs_panel"]')))
+            pub_infos = driver.find_elements(By.XPATH, '//div[@class="gs_qabs_panel"]')
 
-        for pub_info in pub_infos:
+            for pub_info in pub_infos:
+                try:
+                    soup = BeautifulSoup(pub_info.get_attribute('innerHTML'), 'html.parser')
 
-            soup = BeautifulSoup(pub_info.get_attribute('innerHTML'), 'html.parser')
+                    # Extract the title
+                    title_element = soup.find(class_="gs_qabs_title")
+                    title = title_element.get_text(strip=True) if title_element else None
 
-            # Extract the title
-            title_element = soup.find(class_="gs_qabs_title")
-            title = title_element.get_text(strip=True) if title_element else None
+                    # Extract the abstract
+                    abstract_element = soup.find(class_="gs_qabs_snippet")
+                    abstract = abstract_element.get_text(strip=True) if abstract_element else None
 
-            # Extract the abstract
-            abstract_element = soup.find(class_="gs_qabs_snippet")
-            abstract = abstract_element.get_text(strip=True) if abstract_element else None
+                    # Extract the links
+                    links = [a['href'] for a in soup.find_all('a', href=True)]
+                    article_link = links[1] if links else None
 
-            # Extract the links
-            links = [a['href'] for a in soup.find_all('a', href=True)]
-            article_link = links[1] if links else None
+                    # Extract the author names
+                    authors = []
+                    author_element = soup.find(class_="gs_qabs_au2")
+                    authors = author_element.get_text(strip=True) if author_element else None
 
-            # Extract the author names
-            authors = []
-            author_element = soup.find(class_="gs_qabs_au2")
-            authors = author_element.get_text(strip=True) if author_element else None
+                    # Extract the number of citations
+                    citations_element = soup.find(class_="gs_qabs_gsl")
+                    citations = 0
+                    if citations_element:
+                        for li in citations_element.find_all('li'):
+                            if li.text.startswith("Cited by"):
+                                citations = int(li.text.split(" ")[2])
+                                break
 
-            # Extract the number of citations
-            citations_element = soup.find(class_="gs_qabs_gsl")
-            citations = 0
-            if citations_element:
-                for li in citations_element.find_all('li'):
-                    if li.text.startswith("Cited by"):
-                        citations = int(li.text.split(" ")[2])
-                        break
+                    # Append the formatted data to the list
+                    if title and abstract and article_link and authors and citations > 0:
+                        pubs.add((title, abstract, article_link, authors, citations))
+                except Exception as e:
+                    print(f"Error occurred while extracting publication information: {e}")
 
-            # Append the formatted data to the list
-            if title and abstract and article_link and authors and citations > 0:
-                pubs.add((title, abstract, article_link, authors, citations))
-
-        # Go to the next preview
-        next_button = driver.find_element(By.XPATH, '//a[@class="gs_psd_prt"]')
-        next_button.click()
-        time.sleep(0.4)
-        cnt += 1
+            # Go to the next preview
+            next_button = driver.find_element(By.XPATH, '//a[@class="gs_psd_prt"]')
+            next_button.click()
+            time.sleep(0.4)
+            cnt += 1
+        except Exception as e:
+            print(f"Error occurred while navigating to the next preview: {e}")
 
     return pubs
-
 
 # Handle the proxy stuff
 # Format: IP:PORT \n IP:PORT \n ...
