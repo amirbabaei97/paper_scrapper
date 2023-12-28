9c25669526a7f57616d82f7cd74543835fa35f59
AmirBabaei <pr.babayee@icloud.com>
2023-12-28 11:05:26 +0100

    Adding API key validation

--- api.py
+++ api.py
@@ -1,14 +1,33 @@
-from fastapi import FastAPI, HTTPException
+import os
+from fastapi import FastAPI, HTTPException, Request
 from scraper import search_scholar, get_working_proxy, init_proxies
+from dotenv import load_dotenv
 
 app = FastAPI()
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Retrieve the API keys from the environment variable and split into a set
+API_KEYS = set(os.getenv("API_KEYS", "").split(","))
+if not API_KEYS:
+    raise Exception("API_KEYS environment variable is not set.")
 
 @app.on_event("startup")
 async def startup_event():
     init_proxies()
 
+def validate_api_key(api_key: str):
+    """Validate the provided API key."""
+    return api_key in API_KEYS
+
 @app.get("/search/")
-async def search(keyword: str):
+async def search(request: Request, keyword: str):
+    # Extract the API key from the request headers
+    api_key = request.headers.get("x-api-key")
+    if not api_key or not validate_api_key(api_key):
+        raise HTTPException(status_code=401, detail="Invalid or missing API key")
+    
     if not keyword:
         raise HTTPException(status_code=400, detail="Keyword is required")
     
